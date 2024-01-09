ab12b8afcfb5249fbcc6ddd0f8c28e08873eb3f1
ouharri <ouharrioutman@gmail.com>
2024-01-09 11:18:05 +0000

    Repository interface for Apply entities. Extends JpaRepository to inherit common CRUD operations

--- src/main/java/com/jobinow/repositories/ApplyRepository.java
+++ src/main/java/com/jobinow/repositories/ApplyRepository.java
@@ -2,8 +2,16 @@ package com.jobinow.repositories;
 
 import com.jobinow.model.entities.Apply;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+/**
+ * Repository interface for {@link Apply} entities.
+ * Extends JpaRepository to inherit common CRUD operations.
+ *
+ * @author <a href="mailto:ouharri.outman@gmail.com">ouharri</a>
+ */
+@Repository
 public interface ApplyRepository extends JpaRepository<Apply, UUID> {
 }

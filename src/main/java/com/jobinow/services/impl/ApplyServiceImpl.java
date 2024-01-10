f56a7b79f6e16b0a346e0059e8726ec5a7f1ea24
ouharri <ouharrioutman@gmail.com>
2024-01-10 16:30:41 +0000

    Implementation of the ApplyService interface providing functionality for applying to job offers. This class extends the generic service implementation _ServiceImp.

--- src/main/java/com/jobinow/services/impl/ApplyServiceImpl.java
+++ src/main/java/com/jobinow/services/impl/ApplyServiceImpl.java
@@ -3,12 +3,16 @@ package com.jobinow.services.impl;
 import com.jobinow.model.dto.requests.ApplyRequest;
 import com.jobinow.model.dto.responses.ApplyResponse;
 import com.jobinow.model.entities.Apply;
+import com.jobinow.model.entities.User;
 import com.jobinow.model.mapper.ApplyMapper;
 import com.jobinow.repositories.ApplyRepository;
 import com.jobinow.services.spec.ApplyService;
 import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -21,4 +25,25 @@ import java.util.UUID;
 @Service
 @CacheConfig(cacheNames = "apply")
 public class ApplyServiceImpl extends _ServiceImp<UUID, ApplyRequest, ApplyResponse, Apply, ApplyRepository, ApplyMapper> implements ApplyService {
+    /**
+     * Retrieves a paginated list of all job applications for the specified job seeker.
+     *
+     * @param jobSeeker The job seeker for which to retrieve applications.
+     * @param pageable  The pagination information.
+     * @return A paginated list of all job applications for the specified job seeker.
+     */
+    public Page<ApplyResponse> getAllApplies(User jobSeeker, Pageable pageable) {
+        return repository.findAllByJobSeeker(jobSeeker, pageable)
+                .map(mapper::toResponse);
+    }
+
+    /**
+     * Retrieves a list of all job applications for the specified job seeker.
+     *
+     * @param jobSeeker The job seeker for which to retrieve applications.
+     * @return A list of all job applications for the specified job seeker.
+     */
+    public List<ApplyResponse> getAllApplies(User jobSeeker) {
+        return mapper.toResponse(repository.findAllByJobSeeker(jobSeeker));
+    }
 }

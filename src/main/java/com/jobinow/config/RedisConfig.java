5a0471859a7f47129141d037dfd3f56990ae144d
ouharri <ouharrioutman@gmail.com>
2024-01-10 11:20:35 +0000

    Configuration class for Redis And app caching.

--- src/main/java/com/jobinow/config/RedisConfig.java
+++ src/main/java/com/jobinow/config/RedisConfig.java
@@ -20,10 +20,10 @@ import java.time.Duration;
 @EnableCaching
 public class RedisConfig {
 
-    @Value("${redis.host}")
+    @Value("${spring.data.redis.host}")
     private String redisHost;
 
-    @Value("${redis.port}")
+    @Value("${spring.data.redis.port}")
     private int redisPort;
 
     /**
@@ -51,8 +51,6 @@ public class RedisConfig {
         // Build the RedisCacheManager with additional cache configurations for specific caches
         return RedisCacheManager.builder(redisConnectionFactory())
                 .cacheDefaults(cacheConfig)
-                .withCacheConfiguration("tutorials", myDefaultCacheConfig(Duration.ofMinutes(5)))
-                .withCacheConfiguration("tutorial", myDefaultCacheConfig(Duration.ofMinutes(1)))
                 .build();
     }
 
@@ -66,6 +64,9 @@ public class RedisConfig {
         return RedisCacheConfiguration
                 .defaultCacheConfig()
                 .entryTtl(duration)
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair
+                                .fromSerializer(new GenericJackson2JsonRedisSerializer())
+                );
     }
 }

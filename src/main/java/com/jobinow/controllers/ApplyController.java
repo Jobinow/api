0837bfc6af2f13b91b56340ddaaaddb6283f495e
= <itzmohamed007@gmail.com>
2024-01-11 23:08:19 +0100

    updated ApplyController endpoint to respect naming conventions

--- src/main/java/com/jobinow/controllers/ApplyController.java
+++ src/main/java/com/jobinow/controllers/ApplyController.java
@@ -2,18 +2,19 @@ package com.jobinow.controllers;
 
 import com.jobinow.model.dto.requests.ApplyRequest;
 import com.jobinow.model.dto.responses.ApplyResponse;
+import com.jobinow.model.dto.responses.OfferResponse;
 import com.jobinow.model.dto.responses.UserResponses;
+import com.jobinow.model.enums.ApplyType;
 import com.jobinow.services.spec.ApplyService;
 import jakarta.validation.Valid;
+import jakarta.websocket.server.PathParam;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -21,7 +22,8 @@ import java.util.UUID;
 @Slf4j
 @Validated
 @RestController
-@RequestMapping("api/v2/apply")
+@RequiredArgsConstructor
+@RequestMapping("api/v2/applies")
 public class ApplyController extends _Controller<UUID, ApplyRequest, ApplyResponse, ApplyService> {
     @PostMapping("/history")
     public ResponseEntity<List<ApplyResponse>> getHistory(@RequestBody @Valid UserResponses jobSeeker) {

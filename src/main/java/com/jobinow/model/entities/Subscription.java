9b003fc5325149caeaad49bb616a05dd46b3e094
ouharri <ouharrioutman@gmail.com>
2024-01-12 11:24:29 +0000

    Entity class representing a subscription pack.

---
+++ src/main/java/com/jobinow/model/entities/Subscription.java
@@ -0,0 +1,18 @@
+package com.jobinow.model.entities;
+
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@Entity
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Subscription extends AbstractEntity{
+}

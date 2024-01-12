4d248318336e1b121247e1b233e7e6f7ac40f0e9
ouharri <ouharrioutman@gmail.com>
2024-01-12 16:00:53 +0000

    Service implementation class for managing subscription pack. This service extends the generic service interface _Service, specializing in operations related to job pack entities. It defines methods for creating, retrieving, and handling other aspects of job offer data. The interface is designed to work with UUID as the identifier type for job offer entities.

---
+++ src/main/java/com/jobinow/services/spec/PackService.java
@@ -0,0 +1,23 @@
+package com.jobinow.services.spec;
+
+import com.jobinow.model.dto.requests.OfferRequest;
+import com.jobinow.model.dto.responses.OfferResponse;
+
+/**
+ * Service implementation class for managing subscription pack.
+ * This service extends the generic service interface {@link _Service},
+ * specializing in operations related to job pack entities.
+ * It defines methods for creating, retrieving, and handling
+ * other aspects of job offer data.
+ * <p>
+ * The interface is designed to work with UUID as the identifier type for job offer entities.
+ * </p>
+ *
+ * @version 1.0
+ * @see OfferRequest
+ * @see OfferResponse
+ * @see _Service
+ * @author <a href="mailto:ouharri.outman@gmail.com">ouharri</a>
+ */
+public interface PackService {
+}

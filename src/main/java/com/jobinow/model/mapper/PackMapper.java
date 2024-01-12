fd45d34be032fb19a3f361d235a3a7f6955b780e
ouharri <ouharrioutman@gmail.com>
2024-01-12 16:14:09 +0000

    Mapper interface for converting between PackRequest, PackResponse, and Pack entities. Extends the generic _Mapper interface with UUID as the identifier type.

--- src/main/java/com/jobinow/model/mapper/PackMapper.java
+++ src/main/java/com/jobinow/model/mapper/PackMapper.java
@@ -3,6 +3,10 @@ package com.jobinow.model.mapper;
 import com.jobinow.model.dto.requests.PackRequest;
 import com.jobinow.model.dto.responses.PackResponse;
 import com.jobinow.model.entities.Pack;
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
 
 import java.util.UUID;
 
@@ -10,5 +14,10 @@ import java.util.UUID;
  * Mapper interface for converting between {@link PackRequest}, {@link PackResponse}, and {@link Pack} entities.
  * Extends the generic {@link _Mapper} interface with UUID as the identifier type.
  */
+@Mapper(
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+        componentModel = MappingConstants.ComponentModel.SPRING
+)
 public interface PackMapper extends _Mapper<UUID, PackRequest, PackResponse, Pack> {
 }

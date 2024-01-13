748d3a8d294a726c347552aa2096367bcab797b0
ouharri <ouharrioutman@gmail.com>
2024-01-13 14:42:32 +0000

    Spring Data JPA repository for the Subscription entity.

--- src/main/java/com/jobinow/repositories/SubscriptionRepository.java
+++ src/main/java/com/jobinow/repositories/SubscriptionRepository.java
@@ -1,9 +1,12 @@
 package com.jobinow.repositories;
 
 import com.jobinow.model.entities.Subscription;
+import com.jobinow.model.entities.User;
+import com.jobinow.model.enums.SubscriptionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 /**
@@ -13,4 +16,13 @@ import java.util.UUID;
  */
 @Repository
 public interface SubscriptionRepository extends JpaRepository<Subscription, UUID> {
+    /**
+     * Retrieve the latest subscription for a given recruiter with a specified status.
+     *
+     * @param recruiterId The unique identifier of the recruiter associated with the subscription.
+     * @param status      The status of the subscription to filter by.
+     * @return An {@link Optional} containing the latest subscription for the recruiter with the specified status,
+     *         or an empty {@link Optional} if no matching subscription is found.
+     */
+    Optional<Subscription> findFirstByRecruiterAndStatusOrderByCreatedAtDesc(User recruiterId, SubscriptionStatus status);
 }

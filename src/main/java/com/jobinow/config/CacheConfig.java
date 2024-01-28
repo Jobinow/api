549bfac789b4d1fe9b9dbe211a9f73e207e4d73b
ouharri <ouharrioutman@gmail.com>
2024-01-28 18:09:25 +0000

    Configuration class for Redis And app caching.

--- src/main/java/com/jobinow/config/CacheConfig.java
+++ src/main/java/com/jobinow/config/CacheConfig.java
@@ -1,5 +1,6 @@
 package com.jobinow.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
@@ -20,6 +21,12 @@ import java.time.Duration;
 @EnableCaching
 public class CacheConfig {
 
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
     /**
      * Bean definition for the LettuceConnectionFactory, which is responsible for connecting to the Redis server.
      *
@@ -27,7 +34,7 @@ public class CacheConfig {
      */
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(redisHost,redisPort);
 
         return new LettuceConnectionFactory(configuration);
     }

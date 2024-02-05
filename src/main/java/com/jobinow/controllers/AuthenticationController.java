afa295907ddb4fd99cd5ad5c96ffb53aeb840f0c
ouharri <ouharrioutman@gmail.com>
2024-02-05 23:31:58 +0000

    Controller class for handling authentication-related requests. This class provides endpoints for user registration, authentication, token validation, and token refreshing.

--- src/main/java/com/jobinow/controllers/AuthenticationController.java
+++ src/main/java/com/jobinow/controllers/AuthenticationController.java
@@ -1,6 +1,7 @@
 package com.jobinow.controllers;
 
 import com.jobinow.exceptions.ResourceNotCreatedException;
+import com.jobinow.model.dto.Oauth.UrlDto;
 import com.jobinow.model.dto.requests.AuthenticationRequest;
 import com.jobinow.model.dto.requests.RegisterRequest;
 import com.jobinow.model.dto.responses.AuthenticationResponse;
@@ -11,13 +12,11 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -55,6 +54,15 @@ public class AuthenticationController {
         return ResponseEntity.ok(service.register(request));
     }
 
+    /**
+     * Endpoint for registering a job seeker.
+     * Validates the provided request and, if successful, registers the job seeker and generates authentication tokens.
+     *
+     * @param request The registration request containing job seeker details.
+     * @param bindingResult BindingResult for validation errors.
+     * @return ResponseEntity with the registered job seeker's authentication response.
+     * @throws ResourceNotCreatedException If there are validation errors.
+     */
     @PostMapping("/register/job-seeker")
     public ResponseEntity<AuthenticationResponse> registerJobSeeker(
             @Valid @RequestBody final RegisterRequest request,
@@ -66,6 +74,15 @@ public class AuthenticationController {
         return ResponseEntity.ok(service.registerJobSeeker(request));
     }
 
+    /**
+     * Endpoint for registering a manager.
+     * Validates the provided request and, if successful, registers the manager and generates authentication tokens.
+     *
+     * @param request The registration request containing manager details.
+     * @param bindingResult BindingResult for validation errors.
+     * @return ResponseEntity with the registered manager's authentication response.
+     * @throws ResourceNotCreatedException If there are validation errors.
+     */
     @PostMapping("/register/manager")
     public ResponseEntity<AuthenticationResponse> registerManger(
             @Valid @RequestBody final RegisterRequest request,
@@ -77,6 +94,15 @@ public class AuthenticationController {
         return ResponseEntity.ok(service.registerManager(request));
     }
 
+    /**
+     * Endpoint for registering an agent.
+     * Validates the provided request and, if successful, registers the agent and generates authentication tokens.
+     *
+     * @param request The registration request containing agent details.
+     * @param bindingResult BindingResult for validation errors.
+     * @return ResponseEntity with the registered agent's authentication response.
+     * @throws ResourceNotCreatedException If there are validation errors.
+     */
     @PostMapping("/register/agent")
     public ResponseEntity<AuthenticationResponse> registerAgent(
             @Valid @RequestBody final RegisterRequest request,
@@ -88,6 +114,15 @@ public class AuthenticationController {
         return ResponseEntity.ok(service.registerAgent(request));
     }
 
+    /**
+     * Endpoint for registering a recruiter.
+     * Validates the provided request and, if successful, registers the recruiter and generates authentication tokens.
+     *
+     * @param request The registration request containing recruiter details.
+     * @param bindingResult BindingResult for validation errors.
+     * @return ResponseEntity with the registered recruiter's authentication response.
+     * @throws ResourceNotCreatedException If there are validation errors.
+     */
     @PostMapping("/register/recruiter")
     public ResponseEntity<AuthenticationResponse> registerRecruiter(
             @Valid @RequestBody final RegisterRequest request,
@@ -145,5 +180,33 @@ public class AuthenticationController {
             HttpServletResponse response
     ) throws IOException {
         service.refreshToken(request, response);
+    }
+
+    /**
+     * Endpoint for getting the Google OAuth authentication URL.
+     * Returns the URL that users should be redirected to for Google authentication.
+     *
+     * @return ResponseEntity containing the Google OAuth URL.
+     */
+    @GetMapping("/url")
+    public ResponseEntity<UrlDto> googleAuth() {
+        return ResponseEntity.ok(service.getGoogleAuthUrl());
+    }
+
+    /**
+     * Endpoint for handling the callback from Google OAuth.
+     * This method is invoked when Google redirects back to your application after user authentication.
+     *
+     * @param code The authorization code provided by Google.
+     * @return ResponseEntity with the authentication response after processing the Google callback.
+     */
+    @GetMapping("/callback")
+    public ResponseEntity<AuthenticationResponse> callback(@RequestParam("code") final String code) {
+        try {
+            return ResponseEntity.ok(service.authenticateFromGoogleCode(code));
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
     }
 }

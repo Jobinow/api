fe1247532c69173f3abd5b86b43ef62e8f1e69a5
ouharri <ouharrioutman@gmail.com>
2024-02-11 21:06:45 +0000

    Represents a correct answer for a quiz question. Links a specific answer to a question.

--- src/main/java/com/jobinow/model/entities/CorrectAnswer.java
+++ src/main/java/com/jobinow/model/entities/CorrectAnswer.java
@@ -1,16 +1,15 @@
-package com.jobinow.model.entities.quiz;
+package com.jobinow.model.entities;
 
-import com.jobinow.model.entities.AbstractEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
-
 
 /**
  * Represents a correct answer for a quiz question.
@@ -29,13 +28,15 @@ public class CorrectAnswer extends AbstractEntity {
     /**
      * The correct answer for the question.
      */
+    @NotNull(message = "Answer cannot be null")
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "answer_id")
+    @JoinColumn(name = "answer_id", nullable = false)
     private Answer answer;
 
     /**
      * The question associated with this answer.
      */
+    @NotNull(message = "Question cannot be null")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "question_id", nullable = false)
     private Question question;

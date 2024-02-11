ffb92994fb5f27181cd401256c91a621c208bb39
ouharri <ouharrioutman@gmail.com>
2024-02-11 23:21:58 +0000

    Represents an option (Answer) for a question in a quiz.

--- src/main/java/com/jobinow/model/entities/Answer.java
+++ src/main/java/com/jobinow/model/entities/Answer.java
@@ -37,4 +37,11 @@ public class Answer extends AbstractEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "question_id", nullable = false)
     private Question question;
+
+    /**
+     * A flag indicating whether this is the correct answer for the question.
+     */
+    @Column(nullable = false)
+    @NotNull(message = "Correctness flag cannot be null")
+    private boolean isCorrect = false;
 }

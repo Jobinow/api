226cff7fba3236459a4c892ec5d9f852da48d017
ouharri <ouharrioutman@gmail.com>
2024-02-11 21:06:28 +0000

    Represents a badge awarded to a user for achieving a certain score or milestone in a quiz.

--- src/main/java/com/jobinow/model/entities/Badge.java
+++ src/main/java/com/jobinow/model/entities/Badge.java
@@ -1,16 +1,17 @@
-package com.jobinow.model.entities.quiz;
+package com.jobinow.model.entities;
 
-import com.jobinow.model.entities.AbstractEntity;
-import com.jobinow.model.entities.Profil;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.validator.constraints.URL;
 
 import java.util.Set;
 
@@ -30,12 +31,15 @@ public class Badge extends AbstractEntity {
     /**
      * The name of the badge.
      */
+    @NotNull(message = "Badge name cannot be null")
+    @Size(min = 1, max = 255, message = "Badge name must be between 1 and 255 characters")
     @Column(nullable = false)
     private String name;
 
     /**
      * Description of the badge.
      */
+    @Size(max = 500, message = "Badge description cannot exceed 500 characters")
     @Column(length = 500)
     private String description;
 
@@ -43,6 +47,7 @@ public class Badge extends AbstractEntity {
      * The quiz associated with this badge.
      */
     @OneToOne
+    @NotNull(message = "Badge Quiz cannot be null")
     private Quiz quiz;
 
     /**
@@ -54,5 +59,6 @@ public class Badge extends AbstractEntity {
     /**
      * The URL of the badge's image.
      */
+    @URL(message = "Image URL must be valid")
     private String imageUrl;
 }

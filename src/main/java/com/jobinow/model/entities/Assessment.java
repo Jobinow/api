16229b41e1fd9f069d8f2f22e34d4ba911271551
ouharri <ouharrioutman@gmail.com>
2024-02-11 21:06:14 +0000

    Represents an assessment taken by a user. It links a user to a specific quiz and tracks the start and finish times, the percentage result, and whether the assessment was successfully completed.

--- src/main/java/com/jobinow/model/entities/Assessment.java
+++ src/main/java/com/jobinow/model/entities/Assessment.java
@@ -1,14 +1,15 @@
-package com.jobinow.model.entities.quiz;
+package com.jobinow.model.entities;
 
-import com.jobinow.model.entities.AbstractEntity;
-import com.jobinow.model.entities.User;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
@@ -31,17 +32,21 @@ public class Assessment extends AbstractEntity {
      * The quiz associated with this assessment.
      */
     @ManyToOne
+    @NotNull(message = "Quiz cannot be null")
     private Quiz quiz;
 
     /**
      * The user who took this assessment.
      */
     @ManyToOne
+    @NotNull(message = "User cannot be null")
     private User user;
 
     /**
      * The timestamp when the assessment was started.
      */
+    @CreationTimestamp
+    @NotNull(message = "Start time cannot be null")
     private LocalDateTime startedAt;
 
     /**
@@ -52,6 +57,7 @@ public class Assessment extends AbstractEntity {
     /**
      * The percentage result of the assessment.
      */
+    @Min(value = 0, message = "Percentage result cannot be negative")
     private int percentageResult;
 
     /**

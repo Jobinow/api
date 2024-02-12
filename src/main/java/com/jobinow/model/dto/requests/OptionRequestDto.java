41e7e8c8a091ec6b2b29458a58d1575e16bfe721
ouharri <ouharrioutman@gmail.com>
2024-02-12 20:32:43 +0000

    Data Transfer Object (DTO) for creating or updating an Option. This DTO encapsulates the data required to create or modify an answer in a quiz question.

---
+++ src/main/java/com/jobinow/model/dto/requests/OptionRequestDto.java
@@ -0,0 +1,23 @@
+package com.jobinow.model.dto.requests;
+
+import com.jobinow.model.dto.responses.QuestionResponseDto;
+import com.jobinow.model.entities.Option;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+/**
+ * Data Transfer Object (DTO) for creating or updating an {@link Option}.
+ * This DTO encapsulates the data required to create or modify an answer
+ * in a quiz question.
+ */
+public record OptionRequestDto(
+
+        @NotNull(message = "Answer content cannot be null")
+        @Size(message = "Answer content must be between 1 and 500 characters", min = 1, max = 500)
+        String content,
+
+
+        @NotNull(message = "Question cannot be null")
+        QuestionResponseDto question
+) implements _Request {
+}

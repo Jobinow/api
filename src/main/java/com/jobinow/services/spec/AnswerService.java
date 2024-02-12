1950b6b317e70a23a87c243f82027d1b796a0663
ouharri <ouharrioutman@gmail.com>
2024-02-12 20:45:07 +0000

    Service interface for managing operations related to the com.jobinow.model.entities.Answer entity. This interface extends the generic _Service interface, specifying AnswerRequestDto and AnswerResponsesDto for CRUD operations specifically tailored to the Answer entity.

--- src/main/java/com/jobinow/services/spec/AnswerService.java
+++ src/main/java/com/jobinow/services/spec/AnswerService.java
@@ -1,18 +1,19 @@
 package com.jobinow.services.spec;
 
 import com.jobinow.model.dto.requests.AnswerRequestDto;
-import com.jobinow.model.dto.responses.AnswerResponseDto;
-import com.jobinow.model.entities.Answer;
+import com.jobinow.model.dto.responses.AnswerResponsesDto;
 
 import java.util.UUID;
 
 /**
- * Service interface for managing operations related to the {@link Answer} entity.
- * This interface extends the generic _Service interface, specifying AnswerRequestDto and AnswerResponseDto
+ * Service interface for managing operations related to the {@link com.jobinow.model.entities.Answer} entity.
+ * This interface extends the generic _Service interface, specifying AnswerRequestDto and AnswerResponsesDto
  * for CRUD operations specifically tailored to the Answer entity.
  * <p>
- * It provides an abstraction layer above the underlying repository and allows for more complex business logic
- * and interactions that might be required for handling Answer entities.
+ * It provides an abstraction layer above the underlying repository and enables more complex business logic
+ * and interactions required for handling Answer entities.
+ *
+ * @author <a href="mailto:ouharri.outman@gmail.com">ouharri</a>
  */
-public interface AnswerService extends _Service<UUID, AnswerRequestDto, AnswerResponseDto> {
+public interface AnswerService extends _Service<UUID, AnswerRequestDto, AnswerResponsesDto> {
 }

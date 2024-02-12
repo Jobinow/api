8b837dd93b261f479cff43815141709444953f83
ouharri <ouharrioutman@gmail.com>
2024-02-12 20:45:50 +0000

    Implementation of the AnswerService interface. This class provides concrete implementations of CRUD operations for Answer entities. Extends the generic _ServiceImp, utilizing AnswerRepository for data access and AnswerMapper for mapping between entities and DTOs.

--- src/main/java/com/jobinow/services/impl/AnswerServiceImpl.java
+++ src/main/java/com/jobinow/services/impl/AnswerServiceImpl.java
@@ -2,27 +2,22 @@ package com.jobinow.services.impl;
 
 import com.jobinow.mapper.AnswerMapper;
 import com.jobinow.model.dto.requests.AnswerRequestDto;
-import com.jobinow.model.dto.responses.AnswerResponse;
-import com.jobinow.model.dto.responses.AnswerResponseDto;
+import com.jobinow.model.dto.responses.AnswerResponsesDto;
 import com.jobinow.model.entities.Answer;
 import com.jobinow.repositories.AnswerRepository;
-import com.jobinow.services.spec.AnswerService;
-import lombok.AllArgsConstructor;
-import org.springframework.cache.annotation.CacheConfig;
-import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 /**
  * Implementation of the AnswerService interface.
  * This class provides concrete implementations of CRUD operations for {@link Answer} entities.
- * It extends the generic _ServiceImp, utilizing AnswerRepository for data access and AnswerMapper
+ * Extends the generic _ServiceImp, utilizing AnswerRepository for data access and AnswerMapper
  * for mapping between entities and DTOs.
  * <p>
- * The service is annotated with @CacheConfig to leverage caching capabilities for improved performance.
+ * This service handles the business logic associated with Answer entities and bridges the gap
+ * between the controller layer and the data access layer.
+ *
+ * @author <a href="mailto:ouharri.outman@gmail.com">ouharri</a>
  */
-@Service
-@AllArgsConstructor
-@CacheConfig(cacheNames = "Answer")
-public class AnswerServiceImpl extends _ServiceImp<UUID, AnswerRequestDto, AnswerResponseDto, Answer, AnswerRepository, AnswerMapper> implements AnswerService {
+public class AnswerServiceImpl extends _ServiceImp<UUID, AnswerRequestDto, AnswerResponsesDto, Answer, AnswerRepository, AnswerMapper> {
 }

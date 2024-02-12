6320a8e9d4e02262c766aac2eb220fe9fd5483dc
ouharri <ouharrioutman@gmail.com>
2024-02-12 20:34:05 +0000

    Data Transfer Object (DTO) for sending response data related to an Option. This DTO encapsulates the answer data sent back to clients in response to their requests.

--- src/main/java/com/jobinow/model/dto/responses/QuestionResponseDto.java
+++ src/main/java/com/jobinow/model/dto/responses/QuestionResponseDto.java
@@ -41,5 +41,5 @@ public class QuestionResponseDto extends AbstractResponse {
     @Positive(message = "Time must be a positive number")
     int time;
 
-    Set<AnswerResponse> answers;
+    Set<OptionResponse> answers;
 }

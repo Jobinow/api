86c8112bd05904f5234b9936c2f1812fb9bebbea
ouharri <ouharrioutman@gmail.com>
2024-02-12 20:48:35 +0000

    MapStruct mapper interface for Answer entity. This mapper provides functionality for converting between the Answer entity and its corresponding DTOs - AnswerRequestDto and AnswerResponsesDto. It leverages other mappers like QuestionMapper and OptionMapper for related mappings.

--- src/main/java/com/jobinow/mapper/AnswerMapper.java
+++ src/main/java/com/jobinow/mapper/AnswerMapper.java
@@ -1,7 +1,7 @@
 package com.jobinow.mapper;
 
 import com.jobinow.model.dto.requests.AnswerRequestDto;
-import com.jobinow.model.dto.responses.AnswerResponseDto;
+import com.jobinow.model.dto.responses.AnswerResponsesDto;
 import com.jobinow.model.entities.Answer;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
@@ -12,16 +12,18 @@ import java.util.UUID;
 
 /**
  * MapStruct mapper interface for {@link Answer} entity.
- * This mapper provides functionality for converting between {@link Answer} entity
- * and {@link AnswerRequestDto} and {@link AnswerResponseDto}.
- * It leverages {@link QuestionMapper} for question-related mappings.
+ * This mapper provides functionality for converting between the {@link Answer} entity
+ * and its corresponding DTOs - {@link AnswerRequestDto} and {@link AnswerResponsesDto}.
+ * It leverages other mappers like {@link QuestionMapper} and {@link OptionMapper} for related mappings.
+ * <p>
+ * The interface is annotated to indicate the usage of constructor-based injection for Spring,
+ * and to ignore unmapped target properties.
  */
 @Mapper(
         unmappedTargetPolicy = ReportingPolicy.IGNORE,
         injectionStrategy = InjectionStrategy.CONSTRUCTOR,
-        componentModel = MappingConstants.ComponentModel.SPRING
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        uses = {QuestionMapper.class, OptionMapper.class}
 )
-public interface AnswerMapper extends _Mapper<UUID, AnswerRequestDto, AnswerResponseDto, Answer> {
-    // The methods inherited from _Mapper will be automatically implemented by MapStruct,
-    // enabling conversion between Answer entities and DTOs.
+public interface AnswerMapper extends _Mapper<UUID, AnswerRequestDto, AnswerResponsesDto, Answer> {
 }

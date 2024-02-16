836030122946c6d363cafcf936a93de306f55e1b
= <itzmohamed007@gmail.com>
2024-02-16 17:03:21 +0100

    Added Badge Service Implementation

---
+++ src/main/java/com/jobinow/services/impl/BadgeServiceImpl.java
@@ -0,0 +1,14 @@
+package com.jobinow.services.impl;
+
+import com.jobinow.mapper.BadgeMapper;
+import com.jobinow.model.dto.requests.BadgeRequestDto;
+import com.jobinow.model.dto.responses.BadgeResponseDto;
+import com.jobinow.model.entities.Badge;
+import com.jobinow.repositories.BadgeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class BadgeServiceImpl extends _ServiceImp<UUID, BadgeRequestDto, BadgeResponseDto, Badge, BadgeRepository, BadgeMapper> {
+}

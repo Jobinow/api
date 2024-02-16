63fc21b226ea6d239e876b0dbaf5c0eea14dcf3f
= <itzmohamed007@gmail.com>
2024-02-16 19:08:23 +0100

    Corrected BadgeService Bean Bug

--- src/main/java/com/jobinow/services/impl/BadgeServiceImpl.java
+++ src/main/java/com/jobinow/services/impl/BadgeServiceImpl.java
@@ -5,10 +5,11 @@ import com.jobinow.model.dto.requests.BadgeRequestDto;
 import com.jobinow.model.dto.responses.BadgeResponseDto;
 import com.jobinow.model.entities.Badge;
 import com.jobinow.repositories.BadgeRepository;
+import com.jobinow.services.spec.BadgeService;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
-public class BadgeServiceImpl extends _ServiceImp<UUID, BadgeRequestDto, BadgeResponseDto, Badge, BadgeRepository, BadgeMapper> {
+public class BadgeServiceImpl extends _ServiceImp<UUID, BadgeRequestDto, BadgeResponseDto, Badge, BadgeRepository, BadgeMapper> implements BadgeService {
 }

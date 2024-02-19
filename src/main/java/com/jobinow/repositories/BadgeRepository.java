d42140619d72f235925ce4f2db61413fd5515ad9
= <itzmohamed007@gmail.com>
2024-02-19 11:03:24 +0100

    Added a method that retrieves badges that belongs to a certain user (Job Seeker) in BadgeRepository

--- src/main/java/com/jobinow/repositories/BadgeRepository.java
+++ src/main/java/com/jobinow/repositories/BadgeRepository.java
@@ -1,13 +1,22 @@
 package com.jobinow.repositories;
 
+import com.jobinow.model.dto.responses.BadgeResponseDto;
 import com.jobinow.model.entities.Badge;
+import com.jobinow.model.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.Set;
 import java.util.UUID;
 
 /**
  * Spring Data JPA repository for {@link Badge} entities.
  * Facilitates CRUD operations and data retrieval methods for Badge data.
  */
+@Repository
 public interface BadgeRepository extends JpaRepository<Badge, UUID> {
+    @Query("select b from Badge b join b.jobSeekers j where j.id = :userId")
+    Set<Badge> getBadgesByJobSeeker(@Param("userId") UUID userId);
 }

a8f0e9f273d297f9015ae9dc9a1c5ee04a774696
= <itzmohamed007@gmail.com>
2024-02-19 11:05:19 +0100

    Added the endpoint that is responsible for retrieving job seekers badges.

--- src/main/java/com/jobinow/controllers/BadgeController.java
+++ src/main/java/com/jobinow/controllers/BadgeController.java
@@ -5,10 +5,11 @@ import com.jobinow.model.dto.responses.BadgeResponseDto;
 import com.jobinow.services.spec.BadgeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Set;
 import java.util.UUID;
 
 @Slf4j
@@ -17,4 +18,12 @@ import java.util.UUID;
 @RequiredArgsConstructor
 @RequestMapping("api/v2/badges")
 public class BadgeController extends _Controller<UUID, BadgeRequestDto, BadgeResponseDto, BadgeService> {
+    private final BadgeService service;
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<Set<BadgeResponseDto>> getUserBadges(@PathVariable UUID id) {
+        return ResponseEntity.ok(
+                this.service.getBadgesByUserId(id)
+        );
+    }
 }

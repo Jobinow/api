7f4c1f1f5ce7ffefcdc85d984e0e8770213ae97d
= <itzmohamed007@gmail.com>
2024-02-19 11:04:00 +0100

    Added a method that retrieves badges that belongs to a certain user in BadgeService

--- src/main/java/com/jobinow/services/spec/BadgeService.java
+++ src/main/java/com/jobinow/services/spec/BadgeService.java
@@ -2,8 +2,11 @@ package com.jobinow.services.spec;
 
 import com.jobinow.model.dto.requests.BadgeRequestDto;
 import com.jobinow.model.dto.responses.BadgeResponseDto;
+import com.jobinow.model.entities.Badge;
 
+import java.util.Set;
 import java.util.UUID;
 
 public interface BadgeService extends _Service<UUID, BadgeRequestDto, BadgeResponseDto> {
+    Set<BadgeResponseDto> getBadgesByUserId(UUID userId);
 }
